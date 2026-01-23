2d611531ec4b3071da80bdda22c6f0cd8e306571
prabin Saud <prabinsaud2002@gmail.com>
2026-01-23 22:50:53 +0530

    adavance query

--- Query/PRACTICE QUERYS.sql
+++ Query/PRACTICE QUERYS.sql
@@ -28,9 +28,104 @@ LIMIT 5;
 
 
 -- Q8. Show student list with their class (Grade, Section, AcademicYear)
+SELECT
+  s."StudentID",
+  s."AdmissionNo",
+  s."FirstName",
+  s."LastName",
+  c."Grade",
+  c."Section",
+  c."AcademicYear"
+FROM "Students" s
+JOIN "Classes" c
+ON s."ClassID" = c."ClassID"
+ORDER BY s."StudentID";
+
+
 -- Q9. Show attendance list on date 2026-01-10 with student name + status
+SELECT
+  a."AttendanceDate",
+  s."StudentID",
+  s."FirstName",
+  s."LastName",
+  a."Status"
+FROM "Attendance" a
+JOIN "Students" s
+ON a."StudentID" = s."StudentID"
+WHERE a."AttendanceDate" = DATE '2026-01-10'
+ORDER BY s."FirstName", s."LastName";
+
+
 -- Q10. Show total students count class-wise
+SELECT
+  c."ClassID",
+  c."Grade",
+  c."Section",
+  c."AcademicYear",
+  COUNT(s."StudentID") AS "TotalStudents"
+FROM "Classes" c
+LEFT JOIN "Students" s
+ON s."ClassID" = c."ClassID"
+GROUP BY c."ClassID", c."Grade", c."Section", c."AcademicYear"
+ORDER BY "TotalStudents" DESC;
+
+
 -- Q11. Count Present / Absent / Late for SchoolID = 1 on 2026-01-10
+SELECT
+  a."Status",
+  COUNT(*) AS "Total"
+FROM "Attendance" a
+WHERE a."SchoolID" = 1
+AND a."AttendanceDate" = DATE '2026-01-10'
+GROUP BY a."Status"
+ORDER BY "Total" DESC;
+
+
 -- Q12. Show only Absent students on 2026-01-10 with class details
+SELECT
+  s."StudentID",
+  s."FirstName",
+  s."LastName",
+  c."Grade",
+  c."Section",
+  c."AcademicYear",
+  a."AttendanceDate",
+  a."Status"
+FROM "Attendance" a
+JOIN "Students" s
+ON a."StudentID" = s."StudentID"
+JOIN "Classes" c
+ON s."ClassID" = c."ClassID"
+WHERE a."AttendanceDate" = DATE '2026-01-10'
+  AND a."Status" = 'Absent'
+ORDER BY c."Grade", c."Section", s."FirstName";
+
+
 -- Q13. Show class-wise absent count on 2026-01-10
+SELECT
+  c."ClassID",
+  c."Grade",
+  c."Section",
+  COUNT(*) AS "AbsentCount"
+FROM "Attendance" a
+JOIN "Students" s
+  ON a."StudentID" = s."StudentID"
+JOIN "Classes" c
+ON s."ClassID" = c."ClassID"
+WHERE a."AttendanceDate" = DATE '2026-01-10'
+  AND a."Status" = 'Absent'
+GROUP BY c."ClassID", c."Grade", c."Section"
+ORDER BY "AbsentCount" DESC;
+
+
 -- Q14. Show teacher-wise attendance marked count
+SELECT
+  t."TeacherID",
+  t."TeacherName",
+  COUNT(a."AttendanceID") AS "MarkedCount"
+FROM "Teachers" t
+LEFT JOIN "Attendance" a
+ON a."MarkedByTeacherID" = t."TeacherID"
+GROUP BY t."TeacherID", t."TeacherName"
+ORDER BY "MarkedCount" DESC;
+
